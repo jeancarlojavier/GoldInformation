d597ee432485024485bc570851ac0cad9676a3c2
Jeancarlo Javier <jeancarlojavier@gmail.com>
2016-11-10 20:38:12 -0600

    Metiendo intrucciones JS y puliendo detalles

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function ir_c_dweb(){
+  var pagina = document.getElementById("pagina")
+  pagina.src = "Cursos/ProgramacionBasica/index.html"
+}
+
+function ir_c_pbasic(){
+  var pagina = document.getElementById("pagina")
+  pagina.src = "Cursos/DesarrolloWeb/index.html"
+}
